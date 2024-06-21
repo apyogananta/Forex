4ffbef1ed4281bdf316b5233dc48c379437d16bd
DzakySalman <evoyege@gmail.com>
2024-06-21 14:33:39 +0700

    Initial commit

---
+++ app/src/main/java/com/apyogananta/forex/ForexAdapter.java
@@ -0,0 +1,56 @@
+package com.apyogananta.forex;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+public class ForexAdapter extends RecyclerView.Adapter<ForexViewHolder> {
+    private JSONObject _rates;
+    private JSONArray _names;
+
+    public ForexAdapter(JSONObject rates) {
+        this._rates = rates;
+        _names = rates.names();
+    }
+
+    @NonNull
+    @Override
+    public ForexViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.layout_forex, parent, false);
+        return new ForexViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ForexViewHolder holder, int position) {
+        try {
+            String kode = _names.get(position).toString();
+            holder.kodeTextView.setText(kode);
+
+            double kurs = _rates.getDouble(kode);
+            DecimalFormat decimalFormat = new DecimalFormat("###,##0.##");
+            String kurs_2 = decimalFormat.format(kurs);
+            holder.kursTextView.setText(kurs_2);
+        } catch (JSONException e) {
+            Log.e("*apy*", e.getMessage());
+            return;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return _names.length();
+    }
+
+
+}
